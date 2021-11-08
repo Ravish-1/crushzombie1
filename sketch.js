9348ac305ad77a18e613ae8504a871539681b517
Ravish-1 <86870933+Ravish-1@users.noreply.github.com>
2021-11-08 13:21:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+var ground,bridge,leftwall,rightwall
+var jointlink,jointpoint
+var stones=[]
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  engine = Engine.create();
+  world = engine.world;
+  frameRate(80);
+  ground=new Base(0,height-10,width*2,20)
+  leftwall=new Base(300,height/2+60,600,100)
+  rightwall=new Base(width-300,height/2+60,600,100)
+  bridge = new Bridge(15, { x: width / 2 - 400, y: height / 2 });
+   jointpoint = new Base(width - 600, height / 2 + 10, 40, 20);
+   Matter.Composite.add(bridge.body,jointpoint)
+   jointlink=new Link(bridge,jointpoint)
+   for (var i =0; i<= 8; i++){
+     var x = random(width /2-200,width/2+300)
+     var y= random(-10, 140)
+     var stone = new stone(x,y,80,80)
+     stones.push(stone)
+   }
+}
+
+function draw() {
+  background(51);
+  Engine.update(engine);
+
+ground.show()
+bridge.show()
+leftwall.show()
+rightwall.show()
+
+for (var stone of stones){
+  stone.show()
+}
+}
